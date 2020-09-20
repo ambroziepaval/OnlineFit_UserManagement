a2c877816859936d066142beae3044bb4578ed57
Paval, Ambrozie <ambroziepaval@gmail.com>
2020-09-20 21:13:10 +0300

    Add Dockerfile and docker-compose.yml.

--- src/main/java/com/ambroziepaval/usermanager/rest/v1/CreateMockUserData.java
+++ src/main/java/com/ambroziepaval/usermanager/rest/v1/CreateMockUserData.java
@@ -3,47 +3,63 @@ package com.ambroziepaval.usermanager.rest.v1;
 import com.ambroziepaval.usermanager.model.User;
 import com.ambroziepaval.usermanager.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class CreateMockUserData {
+
+    @Value("classpath:static/users.csv")
+    private Resource userResourceFile;
 
     private final UserRepository userRepository;
 
     @PostMapping("/mock/users")
     public void resetUserData() {
 
+        log.info("POST /mock/users REST Request called.");
+
         try {
             List<User> users = getUsersFromFile();
             userRepository.insert(users);
 
         } catch (IOException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Users .csv file not found", e);
+            log.error("Error saving the mock user data.", e);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Users .csv file not found");
         }
     }
 
+    /**
+     * Read all the user information provided in the .csv resource file.
+     * Format: first_name,last_name,email,job_title,race,gender
+     *
+     * @return all the user information mapped directly to the model class User
+     * @throws IOException IO Exception on the users.csv file
+     */
     private List<User> getUsersFromFile() throws IOException {
         List<User> userList = new ArrayList<>();
 
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(loadUserMockFile()));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(userResourceFile.getInputStream()));
 
         String line = bufferedReader.readLine();
         while (Objects.nonNull(line)) {
 
-
             String[] userElements = line.split(",");
             userList.add(new User(userElements[0], userElements[1], userElements[2], userElements[3], userElements[4], userElements[5]));
-
 
             line = bufferedReader.readLine();
         }
@@ -51,11 +67,5 @@ public class CreateMockUserData {
         bufferedReader.close();
 
         return userList;
-    }
-
-    private File loadUserMockFile() throws FileNotFoundException {
-
-        // csv User format: first_name,last_name,email,job_title,race,gender
-        return ResourceUtils.getFile("classpath:static/users.csv");
     }
 }
--- src/main/java/com/ambroziepaval/usermanager/rest/v1/UserController.java
+++ src/main/java/com/ambroziepaval/usermanager/rest/v1/UserController.java
@@ -3,6 +3,7 @@ package com.ambroziepaval.usermanager.rest.v1;
 import com.ambroziepaval.usermanager.model.User;
 import com.ambroziepaval.usermanager.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,15 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @GetMapping("/users")
     public List<User> getUsers() {
+
+        log.info("GET /users REST Request called.");
 
         return userService.getAllUsers();
     }
