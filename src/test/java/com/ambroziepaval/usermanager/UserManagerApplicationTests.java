d0273daa338d49b6dc212b93b1b0327d2489cbd1
Ambrozie Paval <ambrozie.paval@msg.group>
2020-12-29 11:08:24 +0200

    User Management context loading test.

--- src/test/java/com/ambroziepaval/usermanager/UserManagerApplicationTests.java
+++ src/test/java/com/ambroziepaval/usermanager/UserManagerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.ambroziepaval.usermanager;
 
+import com.ambroziepaval.usermanager.rest.v1.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-//@SpringBootTest
+@SpringBootTest
 class UserManagerApplicationTests {
+
+	@Autowired
+	private UserController userController;
 
 	@Test
 	void contextLoads() {
+		assertThat(userController).isNotNull();
 	}
 
 }
